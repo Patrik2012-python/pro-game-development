200dc53486a891606ab4ecfbb6945af26d532d33
Patrik2012-python <peterroman7@gmail.com>
2025-09-15 17:50:43 +0100

    homework

--- lesson_1/lesson1_homework.py
+++ lesson_1/lesson1_homework.py
@@ -8,20 +8,40 @@ running = True
 
 
 class John:
-    def __init__(self,color,radius):
+    def __init__(self,color,w , h , x , y):
         self.color= color
-        self.radius= radius
+        self.width= w
+        self.height= h
+        self.x= x
+        self.y= y
+        self.dimensions= (self.x , self.y, self.width, self.height)
+
     
     def drawrectangle(self):
-        pygame.draw.rect(screen,self.color,(400,400),self.radius)
+        pygame.draw.rect(screen,self.color,self.dimensions)
+
+
+    def growshape(self):
+        self.width+=500
+        self.height+=500
 
 
 
-rectangle1= John('green',30)
 
-while running: 
+ 
+
+
+rectangle1= John('green',356,213,60,200)
+rectangle2= John( 'red', 545,123,120,357)
+
+while running:
+     rectangle1.drawrectangle()
+     rectangle2.drawrectangle()
      for event in pygame.event.get():
          if event.type == pygame.QUIT:
              running = False
+         if event.type == pygame.MOUSEBUTTONDOWN:
+             rectangle1.growshape()
+             
              break
      pygame.display.update()
